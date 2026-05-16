63f6a26b5d0b988d8fe670e043378f974b3549da
Esraa Mohammed Hamza <118177978+esraamohamedhamza@users.noreply.github.com>
2026-05-16 04:02:38 +0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vinxi/config";
+import { defineConfig } from "vinxi";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
