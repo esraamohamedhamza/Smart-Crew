f1fff71f1d67dd72ac25452023bbbd936faa11be
Esraa Mohammed Hamza <118177978+esraamohamedhamza@users.noreply.github.com>
2026-05-16 03:12:35 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// ✅ التعديل هنا: استدعاء الـ Plugin من مسار /vite المخصص له
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath } from "url";
 import path from "path";
+
+// تعويض غياب __dirname في نظام ES Modules الحديث
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
