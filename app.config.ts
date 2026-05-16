2e9663a74121763fa81d8720c011afb3e2234cb5
Esraa Mohammed Hamza <118177978+esraamohamedhamza@users.noreply.github.com>
2026-05-16 04:26:40 +0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,6 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default createApp({
   routers: [
@@ -12,19 +11,20 @@ export default createApp({
       type: "client",
       handler: "./src/start.ts",
       plugins: () => [
-        nodePolyfills({
-          // بنحدد فقط الحزمة اللي مسببة الأزمة من غير ما نلمس الـ process عشان نمنع تضارب unenv
-          include: ["async_hooks"],
-          globals: {
-            Buffer: false,
-            process: false,
-          },
-        }),
         TanStackRouterVite(),
         react(),
         tailwindcss(),
         tsconfigPaths(),
       ],
+      vite: {
+        resolve: {
+          alias: {
+            // تحويل استيراد الـ async_hooks لملف وهمي في المتصفح لمنع خطأ البناء تماماً
+            "node:async_hooks": "unenv/runtime/mock/proxy",
+            "async_hooks": "unenv/runtime/mock/proxy"
+          }
+        }
+      }
     },
   ],
 });
