d2842c808b18607c50cb1aaee33604b1c7348243
Esraa Mohammed Hamza <118177978+esraamohamedhamza@users.noreply.github.com>
2026-05-16 14:08:48 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,9 +2,8 @@ import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   beforeLoad: () => {
-    // التوجيه الصريح لـ Layout الأساسي بتاع الأبلكيشن
     throw redirect({
-      to: '/_app',
+      to: '/_app', // بيوجّه المستخدم لـ دخلة الأبلكيشن الصح
     })
   },
 })
