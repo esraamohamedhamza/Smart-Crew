3f801a647e4943a9f75dcd56980300c479689158
Esraa Mohammed Hamza <118177978+esraamohamedhamza@users.noreply.github.com>
2026-05-16 14:49:30 +0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,37 +1,14 @@
 import { defineConfig } from "vite";
-import { createApp } from "vinxi";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-export default createApp({
-  routers: [
-    {
-      name: "client",
-      type: "client",
-      handler: "./src/start.ts",
-      target: "browser",
-      plugins: () => [
-        TanStackRouterVite(),
-        react(),
-        tailwindcss(),
-        tsconfigPaths(),
-        {
-          name: "skip-async-hooks-for-browser",
-          enforce: "pre",
-          resolveId(source) {
-            if (source === "node:async_hooks" || source === "async_hooks") {
-              return "\0browser-async-hooks";
-            }
-          },
-          load(id) {
-            if (id === "\0browser-async-hooks") {
-              return "export class AsyncLocalStorage { disable() {}; enable() {}; enterWith() {}; run(store, callback) { return callback(); }; getStore() { return undefined; } };";
-            }
-          },
-        },
-      ],
-    },
+export default defineConfig({
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
   ],
 });
