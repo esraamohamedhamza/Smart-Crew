355875f037846ad48c4ce155ee8a13ccf6aaa194
Esraa Mohammed Hamza <118177978+esraamohamedhamza@users.noreply.github.com>
2026-05-16 04:05:53 +0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "@tanstack/react-start/config";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+// بنصدر الكائن الهيكلي مباشرة بدون استخدام دالة defineConfig المقفولة
+export default {
   routers: {
     client: {
       plugins: () => [
@@ -15,4 +15,4 @@ export default defineConfig({
       ],
     },
   },
-});
+};
