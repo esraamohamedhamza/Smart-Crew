3d6e98f741ad0e3b4feb8b8c56b1ef35807fbbdf
Esraa Mohammed Hamza <118177978+esraamohamedhamza@users.noreply.github.com>
2026-05-16 03:55:53 +0300

    Update and rename index.html to app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vinxi/config";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  routers: {
+    client: {
+      plugins: () => [
+        TanStackRouterVite(),
+        react(),
+        tailwindcss(),
+        tsconfigPaths(),
+      ],
+    },
+  },
+});
