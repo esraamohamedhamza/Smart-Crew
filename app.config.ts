f3bc6ae06d3a96fe01ec1eb7344d3b4b686e66eb
Esraa Mohammed Hamza <118177978+esraamohamedhamza@users.noreply.github.com>
2026-05-16 03:20:26 +0300

    Update and rename vite.config.ts to app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,30 +1,29 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vinxi/config";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { fileURLToPath } from "url";
 import path from "path";
 
-// تعويض غياب __dirname في نظام ES Modules الحديث
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vitejs.dev/config/
+// هنا بنعرف الـ App كـ TanStack Start متكامل وليس Vite SPA عادي
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    react(),
-    tailwindcss(),
-    tsconfigPaths(),
-  ],
+  routers: {
+    client: {
+      plugins: () => [
+        TanStackRouterVite(),
+        react(),
+        tailwindcss(),
+        tsconfigPaths(),
+      ],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 8080,
   },
 });
