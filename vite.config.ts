5a855fd1a2139883aadf0f88bcc51f493415652d
Esraa Mohammed Hamza <118177978+esraamohamedhamza@users.noreply.github.com>
2026-05-16 14:56:21 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,21 @@ export default defineConfig({
     react(),
     tailwindcss(),
     tsconfigPaths(),
+    {
+      name: "skip-async-hooks-for-browser",
+      enforce: "pre",
+      resolveId(source) {
+        // لو أي حزمة طلبت المكتبة دي، هنفهم الفايل إنه رايح لملف وهمي
+        if (source === "node:async_hooks" || source === "async_hooks") {
+          return "\0browser-async-hooks";
+        }
+      },
+      load(id) {
+        // بنعمل Fake Export للـ AsyncLocalStorage عشان الفايل يبني بدون أخطاء
+        if (id === "\0browser-async-hooks") {
+          return "export class AsyncLocalStorage { disable() {}; enable() {}; enterWith() {}; run(store, callback) { return callback(); }; getStore() { return undefined; } };";
+        }
+      },
+    },
   ],
 });
