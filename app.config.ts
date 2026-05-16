1fa4ac2257d8fa339f05b294d628d135baf23170
Esraa Mohammed Hamza <118177978+esraamohamedhamza@users.noreply.github.com>
2026-05-16 04:07:17 +0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,15 @@
+import { createApp } from "vinxi";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// بنصدر الكائن الهيكلي مباشرة بدون استخدام دالة defineConfig المقفولة
-export default {
-  routers: {
-    client: {
+export default createApp({
+  routers: [
+    {
+      name: "client",
+      type: "client",
+      handler: "./src/start.ts", // هنا بنربطه بملف الـ start اللي موجود في الـ src عندك
       plugins: () => [
         TanStackRouterVite(),
         react(),
@@ -14,5 +17,5 @@ export default {
         tsconfigPaths(),
       ],
     },
-  },
-};
+  ],
+});
